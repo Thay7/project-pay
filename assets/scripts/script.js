135fa9d689b1a3a7b9ee85936da8f1ec5f100558
Thay7 <thay3x@gmail.com>
2022-11-26 13:39:15 -0300

    add produtos ao carrinho com js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -48,22 +48,22 @@ produtosCarrinho.forEach(function (item) {
 });
 
 
-// const fade = document.querySelector('#fade')
-// const modal = document.querySelector('#modalAdicionarItem')
-// const buttonFecharModal = document.querySelector('#modalAdicionarItem')
+const fade = document.querySelector('#fade')
+const modal = document.querySelector('#modalAdicionarItem')
+const buttonFecharModal = document.querySelector('#modalAdicionarItem')
 
-// function adicionarAoCarrinho() {
-//     modal.classList.toggle('hide')
-//     fade.classList.toggle('hide')
-// }
+function adicionarAoCarrinho() {
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+}
 
-// fade.addEventListener("click", () => {
-//     buttonFecharModal.classList.toggle('hide')
-//     fade.classList.toggle('hide')
-// })
+fade.addEventListener("click", () => {
+    buttonFecharModal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+})
 
-// function fecharModal() {
-//     buttonFecharModal.classList.toggle('hide')
-//     fade.classList.toggle('hide')
-// }
+function fecharModal() {
+    buttonFecharModal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+}
 
