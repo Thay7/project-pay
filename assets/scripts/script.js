4c1223ce67314a509fd8c747d835e1ba9abe4f73
Thay7 <thay3x@gmail.com>
2022-11-25 22:07:46 -0300

    update modal adicionar ao carrinho

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,18 @@
-function adicionarAoCarrinho() {
-    const modal = document.querySelector('#modalAdicionarItem')
-    const fade = document.querySelector('#fade')
+const fade = document.querySelector('#fade')
+const modal = document.querySelector('#modalAdicionarItem')
+const buttonFecharModal = document.querySelector('#modalAdicionarItem')
 
+function adicionarAoCarrinho() {
     modal.classList.toggle('hide')
     fade.classList.toggle('hide')
 }
 
-const fade = document.querySelector('#fade')
-
 fade.addEventListener("click", () => {
-    modal.classList.toggle('hide')
+    buttonFecharModal.classList.toggle('hide')
     fade.classList.toggle('hide')
 })
 
-function fecharModal () {
-const buttonFecharModal = document.querySelector('#buttonFecharModal')
-
-buttonFecharModal.classList.toggle('hide')
-fade.classList.toggle('hide')
-
-
+function fecharModal() {
+    buttonFecharModal.classList.toggle('hide')
+    fade.classList.toggle('hide')
 }
